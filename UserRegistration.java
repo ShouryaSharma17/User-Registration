72421429aed2799a45ba7bbcc21c372fbad7c964
ShouryaSharma17 <shouryasharma17@gmail.com>
2021-04-15 18:30:23 +0530

    Validating Last Name

--- UserRegistration.java
+++ UserRegistration.java
@@ -3,10 +3,10 @@ import java.util.regex.*;
 public class UserRegistration{
 //Check First Name
 	public static void checkFirstName(){
-		System.out.println("Enter a String ");
+		System.out.println("Enter First Name ");
 	        Scanner sc = new Scanner(System.in);
 		String input = sc.nextLine();
-		String p = "^[A-Z]{1}[a-zA-Z]{2}[a-zA-Z]*$"
+		String p = "^[A-Z]{1}[a-zA-Z]{2}[a-zA-Z]*$";
 		if(Pattern.matches(p,input)) {
 		       	System.out.println("Valid First Name "+input);
 	        }
@@ -15,7 +15,22 @@ public class UserRegistration{
 
 		}
 	}
+//Check Last Name
+	public static void checkLastName(){
+		System.out.println("Enter Last Name");
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		String p = "^[A-Z]{1}[a-zA-Z]{2}[a-zA-Z]*$";
+		if(Pattern.matches(p,input)) {
+			System.out.println("Valid Last Name"+input);
+		}
+		else {
+			System.out.println("Please Enter Valid Last Name");
+		}
+	}
+//Calling Method
 	public static void main(String[] args){
 		checkFirstName();
+		checkLastName();
 	}
 }
