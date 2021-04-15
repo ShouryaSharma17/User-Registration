27d2bfc5be754f76ac7f3c70ffd57456aff4d57a
ShouryaSharma17 <shouryasharma17@gmail.com>
2021-04-15 18:55:47 +0530

    Validating Email Address

--- UserRegistration.java
+++ UserRegistration.java
@@ -28,9 +28,24 @@ public class UserRegistration{
 			System.out.println("Please Enter Valid Last Name");
 		}
 	}
+//Check Email
+	public static void checkEmail(){
+		System.out.println("Enter Email");
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		String p = "[a-zA-z]{1,}[.]?[a-z]{1,}?[@]+[a-zA-z]{1,}[.]+[a-z]{1,}[.]?[a-z]{1,}?";
+		if(Pattern.matches(p,input)) {
+		       	System.out.println("Valid Email "+input);
+	        }
+		else {
+		        System.out.println("Please Enter Valid Email");
+
+		}
+	}
 //Calling Method
 	public static void main(String[] args){
 		checkFirstName();
 		checkLastName();
+		checkEmail();
 	}
 }
