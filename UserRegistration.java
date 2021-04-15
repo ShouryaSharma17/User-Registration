21db4290db03e9ed7206f68d3430c42dacd7db07
ShouryaSharma17 <shouryasharma17@gmail.com>
2021-04-15 18:05:59 +0530

    Validating First Name

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,6 +1,21 @@
 import java.util.*;
-class UserRegistration {
-    public static void main(String[] args) {
-        System.out.println("Welcome to User Registration Assignment"); 
-    }
+import java.util.regex.*;
+public class UserRegistration{
+//Check First Name
+	public static void checkFirstName(){
+		System.out.println("Enter a String ");
+	        Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		String p = "^[A-Z]{1}[a-zA-Z]{2}[a-zA-Z]*$"
+		if(Pattern.matches(p,input)) {
+		       	System.out.println("Valid First Name "+input);
+	        }
+		else {
+		        System.out.println("Please Enter Valid First Name");
+
+		}
+	}
+	public static void main(String[] args){
+		checkFirstName();
+	}
 }
